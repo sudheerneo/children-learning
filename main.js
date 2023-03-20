b25a83c8ece46a461eef71c88662767e55d7bcb1
sudheerneo <mailtobsudheer@gmail.com>
2023-03-21 04:01:47 +0530

    updated

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
-const display = document.getElementById("display");
-document.body.onkeyup = (e) => {
-    // a = 65  enter = 13 spacebar = 32 n = 78
-    e.keyCode === 78 && new Start("nmbrs");
-    e.keyCode === 65 && new Start("abcs");
-    (e.keyCode === 32 || e.keyCode === 13) && new Start();
-};
-class Start {
-    constructor(command) {
-        command === "nmbrs" && (this.changeNmbrs(), this.changeColors());
-        command === "abcs" && (this.changeAbcs(), this.changeColors());
-    }
+$(document).ready(() => {
+    $(document).keyup((e) => {
+        (e.key === "r" || e.key === "R" || e.key === "h" || e.key === "H") &&
+            new Command("reset");
+        (e.key === "n" || e.key === "N") && new Command("numbers");
+        (e.key === "a" || e.key === "A") && new Command("abcs");
+        (e.key === " " || e.key === "Enter") && new Command("exec");
+    });
+    new Command().changeColors();
+});
 
+class Command {
+    constructor(req) {
+        const activ = "";
+        const activeState = window.localStorage.getItem("activeState");
+        req && this.changeColors();
+        req === "numbers" && this.numbers();
+        req === "abcs" && this.abcs();
+        req === "reset" &&
+            (window.localStorage.setItem("activeState", "incative"),
+            location.reload());
+        req === "exec" &&
+            (activeState === "numBlock"
+                ? this.guesNumbrsGame()
+                : activeState === "abcBlock"
+                ? this.changeAbcs()
+                : console.log("plese choose any game to play"));
+    }
     randomNmmbr = (max, min) => {
         return Math.floor(Math.random() * (max - min + 1) + min);
     };
@@ -22,8 +37,54 @@ class Start {
                         ${this.randomNmmbr(256, 1)}
                     )`; // ex rgb(222, 154, 57);
     };
-    changeNmbrs = () => {
-        display.innerText = this.randomNmmbr(100, 1);
+    guesNumbrsGame = () => {
+        $("#display").html(`
+            <div class="contaniner numberGame">
+                ${this.randomNmmbr(100, 1)}
+            </div>
+        `);
+    };
+    gueslettersGame = () => {
+        $("#display").html(`
+            <div class="contaniner numberGame">
+                ${this.randomNmmbr(100, 1)}
+            </div>
+        `);
+    };
+    numbers = () => {
+        window.localStorage.setItem("activeState", "numBlock");
+        $("#display").html(`
+           <div class="d-flex flex-wrap align-items-center justify-content-center">
+                <div class="d-flex flex-wrap" id="num">
+                    ${(() => {
+                        let html = "";
+                        for (let i = 1; i <= 105; i++) {
+                            html += `<div class="numbrsList m-1 p-3 text-center" style="background: ${this.randomColor()}; color: ${this.randomColor()}">${i} </div>`;
+                        }
+                        return html;
+                    })()}
+                </div>
+            </div>
+
+        `);
+    };
+    abcs = () => {
+        window.localStorage.setItem("activeState", "abcBlock");
+        $("#display").html(`
+           <div class="  d-flex flex-wrap align-items-center justify-content-center">
+                <div class="d-flex flex-wrap" id="num">
+                    ${(() => {
+                        let html = "";
+                        for (let i = 65; i <= 90; i++) {
+                            html += `<div class="numbrsList m-1 p-3 text-center" style="background: ${this.randomColor()}; color: ${this.randomColor()}">${String.fromCharCode(
+                                i
+                            )} </div>`;
+                        }
+                        return html;
+                    })()}
+                </div>
+            </div>
+        `);
     };
     changeAbcs = () => {
         const bigCase = [
@@ -54,11 +115,31 @@ class Start {
             "YAK",
             "ZEBRA",
         ];
-        display.innerText = bigCase[this.randomNmmbr(25, 0)].toLowerCase();
+        $("#display").html(`
+            <div class="contaniner numberGame">
+                ${bigCase[this.randomNmmbr(25, 0)]}
+            </div>
+        `);
     };
 
     changeColors = () => {
         document.body.style.backgroundColor = this.randomColor();
         document.body.style.color = this.randomColor();
+        $(".navbtns").css({
+            background: this.randomColor(),
+            color: this.randomColor(),
+        });
+        $(".abcsDiv").css({
+            background: this.randomColor(),
+            color: this.randomColor(),
+        });
+        $(".numberDiv").css({
+            background: this.randomColor(),
+            color: this.randomColor(),
+        });
+        $(".navbar").css({
+            background: this.randomColor(),
+            color: this.randomColor(),
+        });
     };
 }
